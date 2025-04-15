3b8dc3bc39b746b125b63cb73d77cb9f34968ff4
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-15 21:25:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -304,7 +304,7 @@ def get_time(seconds):
     return result
     
 async def get_shortlink(link):
-    url = f'{SHORT_URL}/shorten.php'
+    url = f'{SHORT_URL}'
     params = {'api': SHORT_API, 'url': link}
     try:
         async with aiohttp.ClientSession() as session:
