e732a0744b414144248c12da5ecd4902e1e2f0c0
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-20 19:27:01 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -19,17 +19,16 @@ instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
+    _id = fields.StringField()
+    file_ref = fields.StringField(allow_none=True)
+    file_name = fields.StringField(required=True)
+    file_size = fields.IntegerField(required=True)
+    file_type = fields.StringField(allow_none=True)
+    mime_type = fields.StringField(allow_none=True)
+    caption = fields.StringField(allow_none=True)
 
     class Meta:
         collection_name = COLLECTION_NAME
-
 
 async def save_file(media):
     file_id, file_ref = unpack_new_file_id(media.file_id)
